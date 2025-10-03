d236f257eaf881e8e9de4edab58b2a5005b33123
Dhaval Sarsavla <sarsavadhaval003@gmail.com>
2025-10-03 16:01:23 +0530

    Shoping Products

---
+++ JS/cart.js
@@ -0,0 +1,82 @@
+let body = document.getElementById('body');
+
+let CartDisplay = () => {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let str = '';
+    let sum = 0;
+    let qunt = 0;
+
+    cart.forEach(item => {
+        str += `
+            <div class="d-flex flex-wrap col-12 justify-content-center align-items-center border-black-cart my-2 px-2 rounded-2">
+                <div class="col-12 col-md-2 bg-secondary rounded-2 my-2">
+                    <img src="${item.thumbnail}" width="100">
+                </div>
+                <div class="col-12 col-md-4 title my-2">
+                    <p> ${item.title} </p>
+                </div>
+                <div class="col-12 col-md-2 Quantity my-2">
+                    <button class=" btn btn-secondary text-white" onclick="return decqun(${item.id})">-</button>
+                        ${item.quantity}
+                    <button class="btn btn-secondary text-white" onclick="return incqun(${item.id})">+</button>
+                </div>
+                <div class="col-12 col-md-2 price my-2">
+                    <p>Price : ₹${(item.price * item.quantity).toFixed(2)}</p>
+                </div>
+                <div class="col-12 col-md-2 delete-button my-2">
+                    <button class="btn btn-secondary text-white" onclick="return DeleteCartItem(${item.id})"> Delete </button>
+                </div>
+            </div>
+        `;
+        qunt += item.quantity;
+        sum += (item.price * item.quantity);
+    });
+    body.innerHTML = str;
+    if (cart == 0) {
+        body.innerHTML = "Cart Is Empty..";
+    }
+    document.getElementById('total-detail').innerHTML = `<div class="col-6 col-md-4">Quantity : ${qunt}</div><div class="col-6 col-md-2">Price : ₹${sum.toFixed(2)}</div></tr>`;
+}
+
+
+const incqun = (id) => {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let records = cart.map((prd) => {
+        if (id == prd.id) {
+            prd.quantity = prd.quantity += 1;
+            return prd;
+        } else {
+            return prd;
+        }
+    })
+    localStorage.setItem('cart', JSON.stringify(records));
+    CartDisplay();
+}
+
+const decqun = (id) => {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let records = cart.map((prd) => {
+        if (id == prd.id) {
+            if (prd.quantity > 1) {
+                prd.quantity = prd.quantity -= 1;
+            }
+            return prd;
+
+        } else {
+            return prd;
+        }
+    })
+    localStorage.setItem('cart', JSON.stringify(records));
+    CartDisplay();
+}
+
+let DeleteCartItem = (id) => {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let records = cart.filter(prd => {
+        return prd.id != id;
+    })
+    localStorage.setItem('cart', JSON.stringify(records));
+    CartDisplay();
+}
+
+CartDisplay();
